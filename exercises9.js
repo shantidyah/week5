6f7c9fd8275255dd33bf7b970bf19efaa08ec273
shantidyah <shanti.dyah@gmail.com>
2018-04-19 19:35:02 +0700

    cari pelaku

---
+++ exercises9.js
@@ -0,0 +1,11 @@
+function cariPelaku(str) {
+  str=str.match(/abc/g);
+  return str.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
